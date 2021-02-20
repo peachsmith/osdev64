141525c6363c42bd19122caac62de488ee90bdde
John Powell <test@example.com>
2021-02-20 13:27:08 -0600

    cleaned up return from print_bdf

--- src/osdev64/pci.c
+++ src/osdev64/pci.c
@@ -220,13 +220,13 @@ static int print_bdf(uint8_t b, uint8_t d, uint8_t f)
       "| (%2X/%2X/%2X) %-4X  %-4X  %-5X  %-8X  %-6X |\n",
       b, d, f, ven, dev, cls, sub, hea & 0x7F
     );
-  }
-  else
-  {
-    return 0;
+
+    // Return 1 to indicate a multi-function device,
+    // or 2 to indicate a single-function device.
+    return (hea & 0x80) ? 1 : 2;
   }
 
-  return (hea & 0x80) ? 1 : 2;
+  return 0;
 }
 
 static void print_pci()
